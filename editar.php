2b417b888e50679a28a813a15aff5192c265b2b2
Psyckus <93561558+Psyckus@users.noreply.github.com>
2023-02-07 18:51:27 -0600

    Update editar.php

--- editar.php
+++ editar.php
@@ -1,18 +1,18 @@
 <?php include 'template/header.php' ?>
 
 <?php
-    if(!isset($_GET['codigo'])){
+    if(!isset($_GET['telefono'])){
         header('Location: index.php?mensaje=error');
         exit();
     }
 
     include_once 'model/conexion.php';
-    $codigo = $_GET['codigo'];
+    $telefono = $_GET['telefono'];
 
-    $sentencia = $bd->prepare("select * from persona where codigo = ?;");
-    $sentencia->execute([$codigo]);
+    $sentencia = $bd->prepare("SELECT * FROM agenda where telefono = ?;");
+    $sentencia->execute([$telefono]);
     $persona = $sentencia->fetch(PDO::FETCH_OBJ);
-    //print_r($persona);
+    print_r($persona);
 ?>
 
 <div class="container mt-5">
@@ -29,17 +29,32 @@
                         value="<?php echo $persona->nombre; ?>">
                     </div>
                     <div class="mb-3">
+                        <label class="form-label">Apellidos: </label>
+                        <input type="text" class="form-control" name="txtApellidos" autofocus required
+                        value="<?php echo $persona->apellidos; ?>">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Dirección: </label>
+                        <input type="text" class="form-control" name="txtDireccion" autofocus required
+                        value="<?php echo $persona->direccion; ?>">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Telefono: </label>
+                        <input type="text" class="form-control" name="txtTelefono" autofocus required
+                        value="<?php echo $persona->telefono; ?>">
+                    </div>
+                    <div class="mb-3">
                         <label class="form-label">Edad: </label>
                         <input type="number" class="form-control" name="txtEdad" autofocus required
                         value="<?php echo $persona->edad; ?>">
                     </div>
                     <div class="mb-3">
-                        <label class="form-label">Signo: </label>
-                        <input type="text" class="form-control" name="txtSigno" autofocus required
-                        value="<?php echo $persona->signo; ?>">
+                        <label class="form-label">Altura: </label>
+                        <input type="text" class="form-control" name="txtAltura" autofocus required
+                        value="<?php echo $persona->altura; ?>">
                     </div>
                     <div class="d-grid">
-                        <input type="hidden" name="codigo" value="<?php echo $persona->codigo; ?>">
+                        <input type="hidden" name="telefono" value="<?php echo $persona->telefono; ?>">
                         <input type="submit" class="btn btn-primary" value="Editar">
                     </div>
                 </form>
